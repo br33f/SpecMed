0c171e5c9f410e95392eb01c6e55785469db6112
Damian Zamojski <damian.zamojski1@gmail.com>
2017-11-20 18:16:38 +0100

    FE Initial commit

---
+++ FE/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Component} from 'react';
+import {Layout} from './components/Layout.jsx';
+import {Switch, Route} from 'react-router-dom';
+
+export class App extends Component {
+    render() {
+        return (
+          <Switch>
+              <Route exact path='/' component={Layout}/>
+              <Route exact path='/test' component={Layout}/>
+          </Switch>
+        );
+    }
+}
---
+++ FE/app/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Component} from 'react';
+
+export class Layout extends Component {
+    render() {
+        return (
+            <div>Hello world!</div>
+        );
+    }
+}
